86e5773e631ab769834034396bd9696806cf3162
tjperry <tjamesperry@hotmail.com>
2017-09-25 18:25:40 -0500

    Modified tables to reduce number of identity columns that were unused in queries.

--- flyway-4.2.0/sql/01-Designing Base Feature Set/V1_6_1__Create_Moment_Tables.sql
+++ flyway-4.2.0/sql/01-Designing Base Feature Set/V1_6_1__Create_Moment_Tables.sql
@@ -1,10 +1,10 @@
 CREATE TABLE [moment].[Moments] (
 	[ID] BIGINT NOT NULL IDENTITY(1,1),
-	[SenderID] NVARCHAR(64) NOT NULL,
+	[UserID] NVARCHAR(64) NOT NULL,
 	[Latitude] NUMERIC(9,6) NOT NULL,
 	[Longitude] NUMERIC(8,6) NOT NULL,
-	[MediaID] BIGINT NOT NULL,
-	[Public] BIT NOT NULL,
+	[Public] BIT NOT NULL DEFAULT 1,
+	[Hidden] BIT NOT NULL DEFAULT 0,
 	[CreateDate] DATETIME2 NOT NULL DEFAULT GETUTCDATE()	
 );
 
@@ -18,14 +18,14 @@ GO
 ----------------------------------------------------------------------------------------
 
 CREATE TABLE [moment].[Media] (
-	[ID] BIGINT NOT NULL IDENTITY(1,1),
+	[MomentID] BIGINT NOT NULL,
 	[Type] TINYINT NOT NULL,
 	[MediaDir] NVARCHAR(256) NULL,
 	[Message] NVARCHAR(256) NULL
 );
 
 ALTER TABLE [moment].[Media] WITH NOCHECK
-ADD CONSTRAINT PK_Media_ID PRIMARY KEY CLUSTERED ([ID])
+ADD CONSTRAINT PK_Media_MomentID_Type PRIMARY KEY CLUSTERED ([MomentID], [Type])
 WITH(FILLFACTOR = 90);
 
 ALTER TABLE [moment].[Media] WITH CHECK CHECK CONSTRAINT ALL;
@@ -33,32 +33,35 @@ GO
 
 ----------------------------------------------------------------------------------------
 
-CREATE TABLE [moment].[Leaves] (
-	[ID] BIGINT NOT NULL IDENTITY(1,1), 
-	[RecipientID] NVARCHAR(64) NOT NULL,
+CREATE TABLE [moment].[Finds] (
 	[MomentID] BIGINT NOT NULL,
+	[UserID] NVARCHAR(64) NOT NULL,
 	[Found] BIT NOT NULL DEFAULT 0,
-	[FindDate] DATETIME2 NULL,
-	[Shared] BIT NOT NULL DEFAULT 0
+	[FindDate] DATETIME2 NULL
 );
 
-ALTER TABLE [moment].[Leaves] WITH NOCHECK
-ADD CONSTRAINT PK_Leaves_ID PRIMARY KEY CLUSTERED (ID)
+ALTER TABLE [moment].[Finds] WITH NOCHECK
+ADD CONSTRAINT PK_Finds_MomentID_UserID PRIMARY KEY CLUSTERED ([MomentID], [UserID])
 WITH(FILLFACTOR = 90);
 
-ALTER TABLE [moment].[Leaves] WITH CHECK CHECK CONSTRAINT ALL;
+ALTER TABLE [moment].[Finds] WITH CHECK CHECK CONSTRAINT ALL;
 GO
 
 ----------------------------------------------------------------------------------------
 
 CREATE TABLE [moment].[Shares] (
-	[LeaveID] BIGINT NOT NULL,
-	[RecipientID] NVARCHAR(64) NOT NULL
+	[MomentID] BIGINT NOT NULL, 
+	[UserID] NVARCHAR(64) NOT NULL,
+	[All] BIT NOT NULL,
+	[RecipientID] NVARCHAR(64) NULL
 );
 
 ALTER TABLE [moment].[Shares] WITH NOCHECK
-ADD CONSTRAINT PK_Shares_LeaveID_RecipientID PRIMARY KEY CLUSTERED ([LeaveID], [RecipientID])
+ADD CONSTRAINT PK_Shares_MomentID_UserID PRIMARY KEY CLUSTERED ([MomentID], [UserID])
 WITH(FILLFACTOR = 90);
 
 ALTER TABLE [moment].[Shares] WITH CHECK CHECK CONSTRAINT ALL;
 GO
+
+
+
